012f9c01e60240c64abe4c3992577505d6a89246
CHOYUNSIG <cho.yunsig56@gmail.com>
2020-12-23 06:06:57 +0900

    l

--- fft2chord.py
+++ fft2chord.py
@@ -8,8 +8,11 @@ BLUE = (0, 0, 255)
 CHUNK = 2**10
 RATE = 44100
 
-WIDTH = 800
-HEIGHT = 600
+WIDTH_PG = 640
+HEIGHT_PG = 480
+WIDTH_MP = 800
+HEIGHT_MP = 600
+
 FPS = 60
 
 THR_ATK_INIT = 10
@@ -24,10 +27,10 @@ import pygame
 pygame.init()
 pygame.display.set_caption("chord input")
 
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((WIDTH_PG, HEIGHT_PG))
 clock = pygame.time.Clock()
 font50 = pygame.font.Font('com/font/OpenSans-Light.ttf', 50)
-font10 = pygame.font.Font('com/font/OpenSans-Light.ttf', 10)
+font20 = pygame.font.Font('com/font/OpenSans-Light.ttf', 20)
 
 
 import pyaudio
@@ -60,8 +63,8 @@ freqx = freqx[mask]
 
 ####plot generation start
 fig, ax = plt.subplots(2, 1, num='data')
-fig.set_figwidth(WIDTH/fig.dpi)
-fig.set_figheight(HEIGHT/fig.dpi)
+fig.set_figwidth(WIDTH_MP/fig.dpi)
+fig.set_figheight(HEIGHT_MP/fig.dpi)
 
 ax[0].set_title("audio signal")
 ax[0].grid(True)
@@ -93,7 +96,7 @@ def load_data():
     global audio_data, fft_data, max_head, thr_time, thr_accel
     audio_data = np.frombuffer(stream.read(CHUNK), dtype=np.int16)
     fft_data = np.log10(abs(fft(audio_data, n = RATE))/(max_head * CHUNK)*2)[mask]*20
-    
+
     temp_head = np.max(abs(audio_data))
     if max_head <= temp_head:
         thr_accel = max_head = temp_head
@@ -104,6 +107,61 @@ def load_data():
             max_head = max(int(thr_accel * (1 - interval_time / THR_ATK)), temp_head)
 
     ax[0].set_ylim((-max_head, max_head))
+
+
+key = ['z','s','x','d','c', \
+    'v','g','b','h','n','j','m', \
+        ',','l','.',';','/', None]
+chord = ['C','C#','D','Eb','E', \
+    'F','F#','G','Ab','A','Bb','B', \
+        'Cm','C#m','Dm','Ebm','Em', \
+    'Fm','F#m','Gm','Abm','Am','Bbm','Bm']
+
+img_chord = [font50.render(i, True, WHITE) for i in chord]
+img_captured = font20.render('captured', True, WHITE)
+img_movemode = font20.render('Window move mode (Q)', True, WHITE)
+
+pre_pressed = []
+move_mode = True
+
+def pg_input():
+    global pre_pressed, move_mode
+    clock.tick(FPS)
+    screen.fill(BLACK)
+
+    if move_mode:
+        screen.blit(img_movemode, (10, HEIGHT_PG-50))
+    else: 
+        for event in pygame.event.get():
+            pass
+    
+    pressed = [pygame.key.name(k) for k,v in enumerate(pygame.key.get_pressed()) if v]
+    
+    if 'q' in pressed and 'q' not in pre_pressed:
+        move_mode = not move_mode
+    
+    if 'escape' in pressed:
+        quit()
+
+    if move_mode:
+        pass
+    else:
+        for i in range(14):
+            if key[i] in pressed:
+                if key[i+4] in pressed:
+                    screen.blit(img_chord[i%12], (70, 30))
+                    if 'space' in pressed:
+                        screen.blit(img_captured, (170, 70))
+                    break
+                elif key[i+3] in pressed:
+                    screen.blit(img_chord[i%12+12], (70, 30))
+                    if 'space' in pressed:
+                        screen.blit(img_captured, (170, 70))
+                    break
+    
+    pre_pressed = pressed
+    pygame.display.flip()
+
 
 def audio_init():
     audio_line.set_data([], [])
@@ -119,47 +177,11 @@ def audio_animate(i):
     return audio_line,
 
 def fft_animate(i):
+    pg_input()
     fft_line.set_data(freqx, fft_data)
     return fft_line,
-
-key = ['z','s','x','d','c', \
-    'v','g','b','h','n','j','m', \
-        ',','l','.',';','/', None]
-chord = ['C','C#','D','Eb','E', \
-    'F','F#','G','Ab','A','Bb','B', \
-        'Cm','C#m','Dm','Ebm','Em', \
-    'Fm','F#m','Gm','Abm','Am','Bbm','Bm']
-chord_img = [font50.render(i, True, BLACK) for i in chord]
-captuerd_img = font50.render('captured', True, BLACK)
 
 
 animation.FuncAnimation(fig, frames=200, interval=10, blit=True, func=audio_animate, init_func=audio_init)
 animation.FuncAnimation(fig, frames=200, interval=10, blit=True, func=fft_animate, init_func=fft_init)
 plt.show()
-
-'''
-while True:
-    screen.fill(WHITE)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit()
-    
-    pressed = [pygame.key.name(k)for k,v in enumerate(pygame.key.get_pressed()) if v]
-    for i in range(14):
-        if key[i] in pressed:
-            if key[i+4] in pressed:
-                screen.blit(chord_img[i%12], (100, 100))
-                if 'space' in pressed:
-                    screen.blit(captuerd_img, (200, 130))
-                break
-            elif key[i+3] in pressed:
-                screen.blit(chord_img[i%12+12], (100, 100))
-                if 'space' in pressed:
-                    screen.blit(captuerd_img, (200, 130))
-                break
-    
-    pygame.display.flip()
-
-quit()
-'''
